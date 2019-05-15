980edff222c5ffbd08ed2d7def3295e4c89e1291
Michael Harris <mhguitar1997@gmail.com>
2019-05-15 13:49:22 -0700

    Attempted Green Sock animation

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,7 +1,9 @@
 
-const toggleMenu = (event) => {
+const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
-    menu.classList.toggle('menu--open');
+  menu.classList.toggle('menu--open');
+  menu.classList.contains('menu--open') ? TweenMax.to('.menu', .3, {left:0}) : TweenMax.to('.menu', .3, {left:-350});
+  
 }
 
 // Start Here: Create a reference to the ".menu" class
@@ -10,3 +12,13 @@ const menu = document.querySelector('.menu');
 const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener('click', toggleMenu);
+
+function closeMenu(){
+  if(menu.classList.contains('menu--open')){
+    menu.classList.remove('menu--open');
+    TweenMax.to('.menu', .3, {left:-350});
+  }
+}
+
+const articleBox = document.querySelector('.articles');
+articleBox.addEventListener('click', closeMenu);
